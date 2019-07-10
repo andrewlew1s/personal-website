8816b7d8d28442f65651a48a7ddb9f136fb44700
unknown <andrew12lewis@gmail.com>
2019-07-10 10:28:26 -0700

    feat: header

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,8 +1,12 @@
 <template>
 	<div class="landing">
 		<div >
-			<h1 class="landing__centered">Andrew Lewis</h1>
-			<img class="landing__gif" alt="This is an animated gif" src="../assets/circle.gif">
+			<div class="landing__header" >
+				<div class ="landing__header__textbox" >
+					<span class="landing__header__textbox__name">Andrew Lewis</span>
+					<span class="landing__header__textbox__title">Full Stack Engineer</span>
+				</div>
+			</div>
 		</div>
 			<p>CONTENT</p>
 			<p>CONTENT</p>
@@ -30,30 +34,93 @@ export default {
 
 .landing {
 
-	border: 2rem white solid;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	position: relative;
+	border: 1rem white solid;
+	display: block;
 
+	&__header {
+		position: relative;
+		background-repeat: no-repeat;
+		object-fit: cover;
+		height: 95vh;
+		background-position: top;
+		background-size: cover;
+		background-image: linear-gradient(to right bottom, rgba(30, 32, 172, 0.7), rgba(57, 134, 153, 0.6)), url(../assets/circle.gif);
+		clip-path: polygon(0 0, 100% 0, 100% 80vh, 0 100%);
 
-	&__gif {
+		&__textbox {
 
-		max-width: 100%;
-		overflow: hidden;
-		object-fit: contain;
+			position: absolute;
+			color: white;
+			top: 50%;
+			left: 50%;
+			transform: translate( -50%, -50% );
+			text-align: center;
+			text-transform: uppercase;
+			line-height: 1.3;
 
-	}
+			&__title {
+			font-size: 0.5rem;
+			font-weight: 400;
+			letter-spacing: 0.6rem;
+			display: block;
+			animation: rightIn 1.2s ease-out;
+			animation-duration: 1.2s;
+			animation-timing-function: ease-out;
+			animation-delay: 0s;
+			animation-iteration-count: 1;
+			animation-direction: normal;
+			animation-fill-mode: none;
+			animation-play-state: running;
+			animation-name: rightIn;
 
-	&__centered {
+		}
 
-		opacity: 0.7;
-		position: absolute;
-		color: white;
-		top: 35%;
-		left: 50%;
-		transform: translate(-50%, -40%);
-		z-index: 1;
+		@keyframes rightIn {
+			0% {
+				opacity: 0;
+				transform: translateX(5rem);
+			}
+			80% {
+				transform: translateX(-0.5rem);
+			}
+			100% {
+				opacity: 1;
+				transform: translate(0);
+			}
+		}
+
+		&__name {
+			font-size: 1.5rem;
+			font-weight: 400;
+			letter-spacing: 0.6rem;
+			display: block;
+			animation: leftIn 1.2s ease-out;
+			animation-duration: 1.2s;
+			animation-timing-function: ease-out;
+			animation-delay: 0s;
+			animation-iteration-count: 1;
+			animation-direction: normal;
+			animation-fill-mode: none;
+			animation-play-state: running;
+			animation-name: leftIn;
+
+		}
+
+		@keyframes leftIn {
+			0% {
+				opacity: 0;
+				transform: translateX(-5rem);
+			}
+			80% {
+				transform: translateX(0.5rem);
+			}
+			100% {
+				opacity: 1;
+				transform: translate(0);
+			}
+		}
+
+		}
 
 	}
 
@@ -62,9 +129,7 @@ export default {
 @media only screen and (max-width: 800px) {
 
 	.landing__centered {
-		top: 28%;
-		transform: translate(-50%, 50%);
-		font-size: x-large;
+
 	}
 
 }
@@ -72,9 +137,7 @@ export default {
 @media only screen and (max-width: 500px) {
 
 	.landing__centered {
-		top: 13%;
-		transform: translate(-50%, -10%);
-		font-size: small;
+
 	}
 
 }
